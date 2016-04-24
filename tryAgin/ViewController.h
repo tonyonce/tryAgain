a70d4f62ccc0a247e7120b90320d5ee96d78aac2
huangwujie <958865443@qq.com>
2016-04-24 18:26:05 +0800

    git测试

---
+++ tryAgin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tryAgin
+//
+//  Created by Tony on 16/4/24.
+//  Copyright © 2016年 mht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tryAgin/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tryAgin
+//
+//  Created by Tony on 16/4/24.
+//  Copyright © 2016年 mht. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
